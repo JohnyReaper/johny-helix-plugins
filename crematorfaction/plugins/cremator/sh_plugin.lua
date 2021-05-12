4eee5a7c92085c6d6b2565dc0eb7a8f36912f422
JohnyReaper <40298237+JohnyReaper@users.noreply.github.com>
2021-05-12 14:54:58 +0200

    Some fixes

--- crematorfaction/plugins/cremator/sh_plugin.lua
+++ crematorfaction/plugins/cremator/sh_plugin.lua
@@ -53,14 +53,32 @@ end
 
 function PLUGIN:PlayerDeath(client)
 
-	if client:GetModel() == "models/cremator.mdl" then
+	if client:GetModel() == "models/cremator.mdl" or client:GetCharacter():IsCremator() then
 		client:StopSound("npc/cremator/amb_loop.wav")
 	end
 	
 end
 
+if SERVER then
+	function PLUGIN:PlayerFootstep(client, position, foot, soundName, volume)
+
+		if client:GetCharacter():IsCremator() then
+		    local factionTable = ix.faction.Get(client:Team())
+
+		    if (factionTable.walkSounds and (client:IsRunning())) then
+		    	client:EmitSound(factionTable.walkSounds[foot])
+		        return true
+		    elseif (factionTable.walkSounds )then--and (!client:IsRunning())) then
+		        client:EmitSound(factionTable.walkSounds[foot], 75, 100, 0.3, CHAN_AUTO )
+		        return true
+		    
+		    end
+		end
+	end
+end
+
 function PLUGIN:GetPlayerPainSound(client)
-	if (client:GetModel() == "models/cremator.mdl") then
+	if (client:GetModel() == "models/cremator.mdl") or client:GetCharacter():IsCremator() then
 		local PainCremator = {
 			"npc/cremator/amb1.wav",
 			"npc/cremator/amb2.wav",
@@ -72,7 +90,7 @@ function PLUGIN:GetPlayerPainSound(client)
 end
 
 function PLUGIN:GetPlayerDeathSound(client)
-	if (client:GetModel() == "models/cremator.mdl") then
+	if (client:GetModel() == "models/cremator.mdl") or client:GetCharacter():IsCremator() then
 		local crem_die = "npc/cremator/crem_die.wav"
 
 		for k, v in ipairs(player.GetAll()) do
@@ -85,19 +103,21 @@ function PLUGIN:GetPlayerDeathSound(client)
 	end
 end
 
+
 function PLUGIN:PostPlayerLoadout(client) 
 
 	if (client:GetCharacter():IsCremator()) then
 		client:SetMaxHealth(150)
 		client:SetHealth(150)
 		client:SetArmor(150)
+
 	end		
 
 end
 
 function PLUGIN:PlayerSpawn( client )
 
-    if (client:GetModel() == "models/cremator.mdl") then
+    if (client:GetModel() == "models/cremator.mdl") or client:GetCharacter():IsCremator() then
     client:EmitSound( "npc/cremator/amb_loop.wav", 70, 100, 0.6, CHAN_AUTO )
     else
 	client:StopSound("npc/cremator/amb_loop.wav")
